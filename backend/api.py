b618d119b20200bc23109a96bf9d620caa3a74fc
maha-sys <maha142006@gmail.com>
2026-01-02 15:07:33 +0530

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -18,7 +18,10 @@ app = Flask(__name__)
 CORS(app)
 
 # Paths
-MODEL_PATH = '../model/energy_predictor.h5'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, '..', 'model', 'energy_predictor.h5')
+
 UPLOAD_FOLDER = '../uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -443,4 +446,5 @@ def download_pdf_report():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
+
